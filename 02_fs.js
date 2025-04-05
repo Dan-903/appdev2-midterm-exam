b4b24a0c80474be287824b81d31c5ffe82b0b8b9
Dan <latinadaniel4@gmail.com>
2025-04-05 13:23:05 +0800

    Modified Task 2

--- 02_fs.js
+++ 02_fs.js
@@ -7,29 +7,32 @@ const fs = require('fs');
         }
         else{
             console.log("File Created Successfully")
+
+            fs.readFile('message.txt', "utf-8", (err, data) =>{
+                if(err){
+                    console.log(err.message)
+                }
+                else{
+                    console.log(`File Content: ${data}`)
+                    fs.unlink('message.txt', (err) =>{
+                        if(err){
+                            console.log(err.message)
+                        }
+                        else{
+                            console.log("File Deleted Successfully")
+                        }
+                       
+                    })
+                }
+               
+            })
         }
        
     })
 
     
-    fs.readFile('message.txt', "utf-8", (err, data) =>{
-        if(err){
-            console.log(err.message)
-        }
-        else{
-            console.log(`File Content: ${data}`)
-        }
-       
-    })
+    
 
-    fs.unlink('message.txt', (err) =>{
-        if(err){
-            console.log(err.message)
-        }
-        else{
-            console.log("File Deleted Successfully")
-        }
-       
-    })
+    
     
     
