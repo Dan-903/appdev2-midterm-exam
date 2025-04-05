721bd92b607767e07ec69e8574248d96ff73402f
Daniel Latina <latinadaniel4@gmail.com>
2025-04-05 13:11:32 +0800

    Modified Task 3

--- 03_url.js
+++ 03_url.js
@@ -4,10 +4,10 @@ const thisUrl = "https://www.example.com:8080/products?category=electronics&sort
 
 const parsedUrl = url.parse(thisUrl, true)
 
-console.log(parsedUrl.protocol)
-console.log(parsedUrl.hostname)
-console.log(parsedUrl.port)
-console.log(parsedUrl.pathname)
+console.log("Protocol:", parsedUrl.protocol)
+console.log("Hostname:", parsedUrl.hostname)
+console.log("Port:", parsedUrl.port)
+console.log("Pathname:", parsedUrl.pathname)
 console.log("Query Parameter 'Category':", JSON.stringify(parsedUrl.query.category))
 console.log("Query Parameter 'Sort':", JSON.stringify(parsedUrl.query.sort))
 
